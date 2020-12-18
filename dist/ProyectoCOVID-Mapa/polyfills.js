ce9369b55d995c85a27f14f74625b59b56a6a11f
Alfonso Jari <alfonso.jari@gmail.com>
2020-12-18 00:03:52 -0600

    Builded

--- dist/ProyectoCOVID-Mapa/polyfills.js
+++ dist/ProyectoCOVID-Mapa/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\braum\Desktop\ProyectoCOVID-Mapa\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\Users\alfon\Documents\APIS\ProyectoCOVID-Mapa\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
